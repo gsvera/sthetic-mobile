899562da4f97cc5779d27da9a3aa12c8d58c168d
gsvera <55720644+gsvera@users.noreply.github.com>
2025-05-12 15:51:06 -0500

    fix / Refactor props idUser y default objetc para evitar ejecucion de useQuery exceptionDay

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,10 +1,19 @@
 import { AdminCalendarProvider } from "@/components/Modules/AdminCalendarProvider";
+import { getStoreSession, KEY_STORE } from "@/hooks/StoreDataSecure";
+import { useState } from "react";
 import { View } from "react-native";
 
 export default function Calendar() {
+  const [idUser, setIdUser] = useState("");
+
+  getStoreSession({ key: KEY_STORE.idUser }).then(
+    (value) => value && setIdUser(value)
+  );
+  if (!idUser) return <></>;
+
   return (
     <View>
-      <AdminCalendarProvider />
+      <AdminCalendarProvider idUser={idUser} />
     </View>
   );
 }
