bc81d439e1780aad73e095fb1996939766fa41be
gsvera <55720644+gsvera@users.noreply.github.com>
2025-10-29 18:29:27 -0500

    feat / cambio de version

--- constants/Constants.ts
+++ constants/Constants.ts
@@ -1,7 +1,7 @@
 import { weekDays } from "./GeneralTypes";
 
-export const VERSION_ANDROID = "v.1.0.12";
-export const VERSION_IOS = "v.1.0.12";
+export const VERSION_ANDROID = "v.1.0.13";
+export const VERSION_IOS = "v.1.0.13";
 export const APP_NAME_SLUG = 'meredith-aesthetic-work';
 export const PACKAGE_NAME_ANDROID = "com.abasotech.meredithcarework";
 export const APP_STORE_ID = ""; // FALTA EL ID
